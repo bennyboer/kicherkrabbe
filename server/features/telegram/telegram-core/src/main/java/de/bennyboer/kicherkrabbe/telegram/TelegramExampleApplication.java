ab5aa5c31b372b5ffd8c17d021244f953fbd9ddf
bennyboer <barny.eder@gmail.com>
2024-12-29 14:09:24 +0100

    Bootstrapped telegram module

---
+++ server/features/telegram/telegram-core/src/main/java/de/bennyboer/kicherkrabbe/telegram/TelegramExampleApplication.java
@@ -0,0 +1,13 @@
+package de.bennyboer.kicherkrabbe.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramExampleApplication.class, args);
+    }
+
+}
---
+++ server/features/telegram/telegram-starter/src/main/java/de/bennyboer/kicherkrabbe/telegram/TelegramModuleConfig.java
@@ -0,0 +1,8 @@
+package de.bennyboer.kicherkrabbe.telegram;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramModuleConfig {
+
+}
