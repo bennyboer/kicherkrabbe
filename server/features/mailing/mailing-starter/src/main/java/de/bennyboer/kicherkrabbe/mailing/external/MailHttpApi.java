21e720b24fab6d431f4e587cc067339e2eaabf39
bennyboer <barny.eder@gmail.com>
2025-01-02 18:25:17 +0100

    Implemented mailgun api

--- server/features/mailing/mailing-starter/src/main/java/de/bennyboer/kicherkrabbe/mailing/external/MailHttpApi.java
+++ server/features/mailing/mailing-starter/src/main/java/de/bennyboer/kicherkrabbe/mailing/external/MailHttpApi.java
@@ -1,19 +1,48 @@
 package de.bennyboer.kicherkrabbe.mailing.external;
 
+import com.mailgun.api.v3.MailgunMessagesApi;
+import com.mailgun.client.MailgunClient;
+import com.mailgun.model.message.Message;
 import de.bennyboer.kicherkrabbe.mailing.mail.Receiver;
 import de.bennyboer.kicherkrabbe.mailing.mail.Sender;
 import de.bennyboer.kicherkrabbe.mailing.mail.Subject;
 import de.bennyboer.kicherkrabbe.mailing.mail.Text;
+import de.bennyboer.kicherkrabbe.mailing.settings.MailgunApiTokenMissingException;
 import de.bennyboer.kicherkrabbe.mailing.settings.Settings;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Set;
 
 public class MailHttpApi implements MailApi {
 
     @Override
     public Mono<Void> sendMail(Sender sender, Set<Receiver> receivers, Subject subject, Text text, Settings settings) {
-        return Mono.empty(); // TODO
+        if (settings.getMailgun().getApiToken().isEmpty()) {
+            return Mono.error(new MailgunApiTokenMissingException());
+        }
+
+        String apiToken = settings.getMailgun()
+                .getApiToken()
+                .orElseThrow()
+                .getValue();
+
+        MailgunMessagesApi api = MailgunClient.config("https://api.mailgun.net/", apiToken)
+                .createAsyncApi(MailgunMessagesApi.class);
+
+        List<String> receiverMails = receivers.stream()
+                .map(receiver -> receiver.getMail().getValue())
+                .toList();
+
+        var message = Message.builder()
+                .from(sender.getMail().getValue())
+                .to(receiverMails)
+                .subject(subject.getValue())
+                .text(text.getValue())
+                .build();
+
+        return Mono.fromFuture(api.sendMessageAsync("sandbox01b5b630b21c405a90559e7de7b756f8.mailgun.org", message))
+                .then();
     }
 
 }
