bd2023159572b73a27f90e84f57a54ed6d06ae7f
bennyboer <barny.eder@gmail.com>
2026-01-31 22:20:13 +0100

    Using samples for PublishedPattern and PublishedFabric

--- backend/features/fabrics/fabrics-example/src/test/java/de/bennyboer/kicherkrabbe/fabrics/http/QueryFeaturedFabricsHttpHandlerTest.java
+++ backend/features/fabrics/fabrics-example/src/test/java/de/bennyboer/kicherkrabbe/fabrics/http/QueryFeaturedFabricsHttpHandlerTest.java
@@ -2,12 +2,10 @@ package de.bennyboer.kicherkrabbe.fabrics.http;
 
 import de.bennyboer.kicherkrabbe.eventsourcing.event.metadata.agent.Agent;
 import de.bennyboer.kicherkrabbe.eventsourcing.event.metadata.agent.AgentId;
-import de.bennyboer.kicherkrabbe.fabrics.*;
 import de.bennyboer.kicherkrabbe.fabrics.http.api.responses.QueryFeaturedFabricsResponse;
+import de.bennyboer.kicherkrabbe.fabrics.samples.SamplePublishedFabric;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -19,17 +17,7 @@ public class QueryFeaturedFabricsHttpHandlerTest extends HttpHandlerTest {
         var token = createTokenForUser("USER_ID");
 
         when(module.getFeaturedFabrics(Agent.user(AgentId.of("USER_ID")))).thenReturn(Flux.just(
-                PublishedFabric.of(
-                        FabricId.of("FABRIC_ID"),
-                        FabricName.of("Ice bear party"),
-                        ImageId.of("IMAGE_ID"),
-                        Set.of(ColorId.of("COLOR_ID")),
-                        Set.of(TopicId.of("TOPIC_ID")),
-                        Set.of(FabricTypeAvailability.of(
-                                FabricTypeId.of("JERSEY_ID"),
-                                true
-                        ))
-                )
+                SamplePublishedFabric.builder().build().toModel()
         ));
 
         var exchange = client.get()
@@ -41,31 +29,12 @@ public class QueryFeaturedFabricsHttpHandlerTest extends HttpHandlerTest {
 
         var response = exchange.expectBody(QueryFeaturedFabricsResponse.class).returnResult().getResponseBody();
         assertThat(response.fabrics).hasSize(1);
-
-        var fabric = response.fabrics.get(0);
-        assertThat(fabric.id).isEqualTo("FABRIC_ID");
-        assertThat(fabric.name).isEqualTo("Ice bear party");
-        assertThat(fabric.imageId).isEqualTo("IMAGE_ID");
-        assertThat(fabric.colorIds).containsExactly("COLOR_ID");
-        assertThat(fabric.topicIds).containsExactly("TOPIC_ID");
-        assertThat(fabric.availability).hasSize(1);
-
-        var availability = fabric.availability.stream().findFirst().get();
-        assertThat(availability.typeId).isEqualTo("JERSEY_ID");
-        assertThat(availability.inStock).isTrue();
     }
 
     @Test
     void shouldAllowUnauthorizedAccess() {
         when(module.getFeaturedFabrics(Agent.anonymous())).thenReturn(Flux.just(
-                PublishedFabric.of(
-                        FabricId.of("FABRIC_ID"),
-                        FabricName.of("Ice bear party"),
-                        ImageId.of("IMAGE_ID"),
-                        Set.of(ColorId.of("COLOR_ID")),
-                        Set.of(TopicId.of("TOPIC_ID")),
-                        Set.of()
-                )
+                SamplePublishedFabric.builder().build().toModel()
         ));
 
         var exchange = client.get()
@@ -76,14 +45,6 @@ public class QueryFeaturedFabricsHttpHandlerTest extends HttpHandlerTest {
 
         var response = exchange.expectBody(QueryFeaturedFabricsResponse.class).returnResult().getResponseBody();
         assertThat(response.fabrics).hasSize(1);
-
-        var fabric = response.fabrics.get(0);
-        assertThat(fabric.id).isEqualTo("FABRIC_ID");
-        assertThat(fabric.name).isEqualTo("Ice bear party");
-        assertThat(fabric.imageId).isEqualTo("IMAGE_ID");
-        assertThat(fabric.colorIds).containsExactly("COLOR_ID");
-        assertThat(fabric.topicIds).containsExactly("TOPIC_ID");
-        assertThat(fabric.availability).isEmpty();
     }
 
     @Test
--- backend/features/patterns/patterns-example/src/test/java/de/bennyboer/kicherkrabbe/patterns/http/QueryFeaturedPatternsHttpHandlerTest.java
+++ backend/features/patterns/patterns-example/src/test/java/de/bennyboer/kicherkrabbe/patterns/http/QueryFeaturedPatternsHttpHandlerTest.java
@@ -2,14 +2,10 @@ package de.bennyboer.kicherkrabbe.patterns.http;
 
 import de.bennyboer.kicherkrabbe.eventsourcing.event.metadata.agent.Agent;
 import de.bennyboer.kicherkrabbe.eventsourcing.event.metadata.agent.AgentId;
-import de.bennyboer.kicherkrabbe.money.Money;
-import de.bennyboer.kicherkrabbe.patterns.*;
 import de.bennyboer.kicherkrabbe.patterns.http.api.responses.QueryFeaturedPatternsResponse;
+import de.bennyboer.kicherkrabbe.patterns.samples.SamplePublishedPattern;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-
-import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -21,32 +17,7 @@ public class QueryFeaturedPatternsHttpHandlerTest extends HttpHandlerTest {
         var token = createTokenForUser("USER_ID");
 
         when(module.getFeaturedPatterns(Agent.user(AgentId.of("USER_ID")))).thenReturn(Flux.just(
-                PublishedPattern.of(
-                        PatternId.of("PATTERN_ID"),
-                        PatternName.of("Summerdress"),
-                        PatternNumber.of("S-D-SUM-1"),
-                        PatternDescription.of("A beautiful summer dress"),
-                        PatternAlias.of("summerdress"),
-                        PatternAttribution.of(
-                                OriginalPatternName.of("Summerdress EXTREME"),
-                                PatternDesigner.of("EXTREME PATTERNS inc.")
-                        ),
-                        Set.of(PatternCategoryId.of("DRESS_ID")),
-                        List.of(ImageId.of("IMAGE_ID")),
-                        List.of(PatternVariant.of(
-                                PatternVariantName.of("Normal"),
-                                Set.of(PricedSizeRange.of(
-                                        80,
-                                        86L,
-                                        null,
-                                        Money.euro(2000)
-                                ))
-                        )),
-                        List.of(PatternExtra.of(
-                                PatternExtraName.of("Sewing instructions"),
-                                Money.euro(200)
-                        ))
-                )
+                SamplePublishedPattern.builder().build().toModel()
         ));
 
         var exchange = client.get()
@@ -58,62 +29,12 @@ public class QueryFeaturedPatternsHttpHandlerTest extends HttpHandlerTest {
 
         var response = exchange.expectBody(QueryFeaturedPatternsResponse.class).returnResult().getResponseBody();
         assertThat(response.patterns).hasSize(1);
-
-        var pattern = response.patterns.get(0);
-        assertThat(pattern.id).isEqualTo("PATTERN_ID");
-        assertThat(pattern.name).isEqualTo("Summerdress");
-        assertThat(pattern.number).isEqualTo("S-D-SUM-1");
-        assertThat(pattern.description).isEqualTo("A beautiful summer dress");
-        assertThat(pattern.alias).isEqualTo("summerdress");
-        assertThat(pattern.attribution.originalPatternName).isEqualTo("Summerdress EXTREME");
-        assertThat(pattern.attribution.designer).isEqualTo("EXTREME PATTERNS inc.");
-        assertThat(pattern.categories).containsExactly("DRESS_ID");
-        assertThat(pattern.images).containsExactly("IMAGE_ID");
-        assertThat(pattern.variants).hasSize(1);
-
-        var variant = pattern.variants.get(0);
-        assertThat(variant.name).isEqualTo("Normal");
-        assertThat(variant.pricedSizeRanges).hasSize(1);
-
-        var pricedSizeRange = variant.pricedSizeRanges.stream().findFirst().get();
-        assertThat(pricedSizeRange.from).isEqualTo(80);
-        assertThat(pricedSizeRange.to).isEqualTo(86);
-        assertThat(pricedSizeRange.price.amount).isEqualTo(2000);
-        assertThat(pricedSizeRange.price.currency).isEqualTo("EUR");
-
-        assertThat(pattern.extras).hasSize(1);
-        var extra = pattern.extras.get(0);
-        assertThat(extra.name).isEqualTo("Sewing instructions");
-        assertThat(extra.price.amount).isEqualTo(200);
-        assertThat(extra.price.currency).isEqualTo("EUR");
     }
 
     @Test
     void shouldAllowUnauthorizedAccess() {
         when(module.getFeaturedPatterns(Agent.anonymous())).thenReturn(Flux.just(
-                PublishedPattern.of(
-                        PatternId.of("PATTERN_ID"),
-                        PatternName.of("Summerdress"),
-                        PatternNumber.of("S-D-SUM-1"),
-                        null,
-                        PatternAlias.of("summerdress"),
-                        PatternAttribution.of(
-                                OriginalPatternName.of("Summerdress EXTREME"),
-                                PatternDesigner.of("EXTREME PATTERNS inc.")
-                        ),
-                        Set.of(PatternCategoryId.of("DRESS_ID")),
-                        List.of(ImageId.of("IMAGE_ID")),
-                        List.of(PatternVariant.of(
-                                PatternVariantName.of("Normal"),
-                                Set.of(PricedSizeRange.of(
-                                        80,
-                                        86L,
-                                        null,
-                                        Money.euro(2000)
-                                ))
-                        )),
-                        List.of()
-                )
+                SamplePublishedPattern.builder().build().toModel()
         ));
 
         var exchange = client.get()
@@ -124,11 +45,6 @@ public class QueryFeaturedPatternsHttpHandlerTest extends HttpHandlerTest {
 
         var response = exchange.expectBody(QueryFeaturedPatternsResponse.class).returnResult().getResponseBody();
         assertThat(response.patterns).hasSize(1);
-
-        var pattern = response.patterns.get(0);
-        assertThat(pattern.id).isEqualTo("PATTERN_ID");
-        assertThat(pattern.name).isEqualTo("Summerdress");
-        assertThat(pattern.alias).isEqualTo("summerdress");
     }
 
     @Test
