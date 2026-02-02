36bbd525f0f26b2543ac768bddd50336e4b0118d
bennyboer <barny.eder@gmail.com>
2026-02-02 16:37:21 +0100

    Management app separate deployment

---
+++ frontend/management/server.mjs
@@ -0,0 +1,23 @@
+import express from "express";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const browserDistFolder = join(__dirname, "dist/management/browser");
+
+app.use(
+	express.static(browserDistFolder, {
+		maxAge: "1y",
+		index: false,
+	}),
+);
+
+app.get("/{*path}", (req, res) => {
+	res.sendFile(join(browserDistFolder, "index.html"));
+});
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => {
+	console.log(`Management app listening on http://localhost:${port}`);
+});
